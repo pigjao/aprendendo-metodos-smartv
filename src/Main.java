e9430833e2e16a5c64bf544462b8860c98c525de
Pigjao <joaovictorbagomes@gmail.com>
2022-10-19 21:15:20 -0300

    Atualizar código

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SmartTv smartTv = new SmartTv();
+
+            System.out.println("A tv está ligada? " + smartTv.ligada);
+            System.out.println("O volume atual da TV é: " + smartTv.volume);
+            System.out.println("O canal atual da TV é: " + smartTv.canalDigital);
+
+            smartTv.aumentarVolume();
+            smartTv.mudarCanal(8.1);
+            smartTv.desligar();
+            smartTv.ligar();
+            smartTv.subirCanal();
+            smartTv.subirCanal();
+            smartTv.abaixarVolume();
+            smartTv.descerCanal();
+            smartTv.volumeMaximo();
+            
+
+
+
     }
 }
--- src/SmartTv.java
+++ src/SmartTv.java
@@ -1,2 +1,48 @@
-package PACKAGE_NAME;public class SmartTv {
+public class SmartTv {
+    boolean ligada = false;
+    double canalDigital = 10.1;
+    int volume = 30;
+
+    public void ligar(){
+        ligada = true;
+        System.out.println("A tv está ligada!");
+    }
+    public void desligar(){
+        ligada = false;
+        System.out.println("A tv está desligada!");
+    }
+    public void aumentarVolume(){
+        //volume = volume + 1;
+        volume++;
+        System.out.println("O volume da tv agora é: " +  volume);
+    }
+    public void abaixarVolume(){
+        //volume = volume - 1;
+        volume--;
+        System.out.println("O volume da tv agora é: " +  volume);
+    }
+    public void volumeMaximo (){
+        volume = 100;
+        System.out.println("O volume da tv está no máximo: " +  volume);
+    }
+
+    public void volumeMinimo (){
+        volume = 0;
+        System.out.println("O volume da tv está no mínimo: " +  volume);
+    }
+
+    public void subirCanal(){
+        //canalDigital = canalDigital + 1;
+        canalDigital++;
+        System.out.println("O canal da tv agora é: " +  canalDigital);
+    }
+    public void descerCanal(){
+        //canalDigital = canalDigital -1;
+        canalDigital--;
+        System.out.println("O canal da tv agora é: " +  canalDigital);
+    }
+    public void mudarCanal(double novoCanal){
+        canalDigital = novoCanal;
+        System.out.println("O canal da tv agora é: " +  canalDigital);
+    }
 }
